ef7949c1855ca0867c8e422ac34a742d26218398
todo996 <kuquan55555@gmail.com>
2026-08-06 11:33:42 +0700

    Cho phép tên nhân vật tiếng Việt trong model

--- server/slgserver/model/role.go
+++ server/slgserver/model/role.go
@@ -8,23 +8,23 @@ import (
 )
 
 type Role struct {
-	RId			int			`xorm:"rid pk autoincr"`
-	UId			int			`xorm:"uid"`
-	NickName	string		`xorm:"nick_name" validate:"min=4,max=20,regexp=^[a-zA-Z0-9_]*$"`
-	Balance		int			`xorm:"balance"`
-	HeadId		int16		`xorm:"headId"`
-	Sex			int8		`xorm:"sex"`
-	Profile		string		`xorm:"profile"`
-	LoginTime   time.Time	`xorm:"login_time"`
-	LogoutTime  time.Time	`xorm:"logout_time"`
-	CreatedAt	time.Time	`xorm:"created_at"`
+	RId       int       `xorm:"rid pk autoincr"`
+	UId       int       `xorm:"uid"`
+	NickName  string    `xorm:"nick_name" validate:"min=2,max=20"`
+	Balance   int       `xorm:"balance"`
+	HeadId    int16     `xorm:"headId"`
+	Sex       int8      `xorm:"sex"`
+	Profile   string    `xorm:"profile"`
+	LoginTime time.Time `xorm:"login_time"`
+	LogoutTime time.Time `xorm:"logout_time"`
+	CreatedAt time.Time `xorm:"created_at"`
 }
 
 func (this *Role) TableName() string {
 	return "tb_role" + fmt.Sprintf("_%d", ServerId)
 }
 
-func (this *Role) ToProto() interface{}{
+func (this *Role) ToProto() interface{} {
 	p := proto.Role{}
 	p.UId = this.UId
 	p.RId = this.RId
