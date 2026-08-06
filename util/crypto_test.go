3d065f7d82a949b89c6f2ae248e38e3172029476
todo996 <kuquan55555@gmail.com>
2026-08-06 11:55:32 +0700

    Kiểm thử nâng cấp mật khẩu qua mọi giai đoạn

--- util/crypto_test.go
+++ util/crypto_test.go
@@ -3,7 +3,7 @@ package util
 import "testing"
 
 func TestHashAndVerifyPassword(t *testing.T) {
-	password := "md5-client-value"
+	password := "MatKhau-AnToan-2026"
 	hash, err := HashPassword(password)
 	if err != nil {
 		t.Fatalf("HashPassword() lỗi: %v", err)
@@ -26,16 +26,48 @@ func TestHashAndVerifyPassword(t *testing.T) {
 	}
 }
 
-func TestVerifyLegacyPasswordRequiresUpgrade(t *testing.T) {
-	password := "legacy-client-md5"
+func TestVerifyLegacyPasswordFromNewClientRequiresUpgrade(t *testing.T) {
+	password := "MatKhau-Cu-2024"
 	passcode := "abc123"
-	legacyHash := Password(password, passcode)
+	legacyHash := Password(Md5(password), passcode)
 
 	valid, needsUpgrade := VerifyPassword(password, legacyHash, passcode)
 	if !valid {
-		t.Fatal("tài khoản MD5 cũ phải đăng nhập được")
+		t.Fatal("tài khoản MD5 cũ phải đăng nhập được bằng client mới")
 	}
 	if !needsUpgrade {
 		t.Fatal("tài khoản MD5 cũ phải được đánh dấu cần nâng cấp")
+	}
+}
+
+func TestVerifyLegacyPasswordFromOldClientRequiresUpgrade(t *testing.T) {
+	password := "MatKhau-Cu-2024"
+	clientMD5 := Md5(password)
+	passcode := "abc123"
+	legacyHash := Password(clientMD5, passcode)
+
+	valid, needsUpgrade := VerifyPassword(clientMD5, legacyHash, passcode)
+	if !valid {
+		t.Fatal("client MD5 cũ phải tiếp tục đăng nhập được trong thời gian chuyển đổi")
+	}
+	if !needsUpgrade {
+		t.Fatal("client MD5 cũ phải được đánh dấu cần nâng cấp")
+	}
+}
+
+func TestVerifyTransitionalBcryptRequiresUpgrade(t *testing.T) {
+	password := "MatKhau-Chuyen-Tiep"
+	clientMD5 := Md5(password)
+	transitionalHash, err := HashPassword(clientMD5)
+	if err != nil {
+		t.Fatalf("không tạo được bcrypt chuyển tiếp: %v", err)
+	}
+
+	valid, needsUpgrade := VerifyPassword(password, transitionalHash, "")
+	if !valid {
+		t.Fatal("bcrypt chuyển tiếp của MD5 phải được chấp nhận")
+	}
+	if !needsUpgrade {
+		t.Fatal("bcrypt chuyển tiếp phải được nâng cấp sang bcrypt mật khẩu gốc")
 	}
 }
